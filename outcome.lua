ef760393e39523e95a0363c4d695d43de3902f40
troy-beep <krish.disbound@gmail.com>
2024-12-07 23:20:27 +0530

    Update outcome.lua

--- outcome.lua
+++ outcome.lua
@@ -1,18 +1,35 @@
 local bint = require('.bint')(256)
 local ao = require('.ao')
+--[[
+  This module implements the ao Standard Token Specification.
 
+  Terms:
+    Sender: the wallet or Process that sent the Message
+
+  It will first initialize the internal state, and then attach handlers,
+    according to the ao Standard Token Spec API:
+
+    - Info(): return the token parameters, like Name, Ticker, Logo, and Denomination
+
+    - Balance(Target?: string): return the token balance of the Target. If Target is not provided, the Sender
+        is assumed to be the Target
+
+    - Balances(): return the token balance of all participants
+
+    - Transfer(Target: string, Quantity: number): if the Sender has a sufficient balance, send the specified Quantity
+        to the Target. It will also issue a Credit-Notice to the Target and a Debit-Notice to the Sender
+
+    - Mint(Quantity: number): if the Sender matches the Process Owner, then mint the desired Quantity of tokens, adding
+        them the Processes' balance
+]]
+--
 local json = require('json')
 
-local pretty = require(".pretty")
+--[[
+  utils helper functions to remove the bint complexity.
+]]
+--
 
-local formatted = pretty.tprint({
-  name = "John Doe",
-  age = 22,
-  friends = { "Maria", "Victor" }
-}, 2)
-
--- prints the formatted table structure
-print(formatted)
 local utils = {
   add = function(a, b)
     return tostring(bint(a) + bint(b))
@@ -28,7 +45,13 @@ local utils = {
   end
 }
 
+--[[
+     Initialize State
 
+     ao.id is equal to the Process.Id
+   ]]
+--
+-- @dev used to reset state between integration tests
 ResetState = true
 
 --[[
