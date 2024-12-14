8300fdccce03b01fff06e7a8d1f082f75908d268
troy-beep <krish.disbound@gmail.com>
2024-12-14 21:19:52 +0530

    Update s3xy.lua

--- s3xy.lua
+++ s3xy.lua
@@ -17,9 +17,6 @@ function missing_return_example(x, y) local result = x + y end
 function reentrancy_example() external_call() state_variable = state_variable + 1 end
 function private_key_exposure_example() end -- Potential exposure of private key
 setfenv = nil getfenv = nil -- Deprecated function usage
-
-
-
 function perform_expensive_operation() for i = 1, 1000000 do end end
 function unchecked_external_call_example() local result = some_external_contract.some_function() end -- No check on the result of the external call
 function greedy_function() transfer(some_address, 1000) end -- Fund transfer without conditions
@@ -29,6 +26,8 @@ function call_external() some_contract.some_method() end -- No checks on the cal
 function unsafe_transfer() transfer(some_address, 1000) end -- Function with a potential greedy/suicidal transfer
 function deprecated_pragma() setfenv(1, {}) getfenv(1) end -- Function with deprecated pragma usage
 function expensive_op() perform_expensive_operation() end -- Function performing an expensive operation
+
+
 
 
 
